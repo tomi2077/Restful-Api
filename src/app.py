aa13239ad1a62d7abb23dd3e79e5fa61a2c4a349
Rails Udemy Course <tominiyiajayi@gmail.com>
2020-05-03 00:23:05 +0100

    First Commit of Restful API Tutorail

---
+++ src/app.py
@@ -0,0 +1,35 @@
+#src/app.py
+
+from flask import Flask
+
+from .config import app_config
+from .models import db, bcrypt
+
+from .views.UserView import user_api as user_blueprint # add this line
+
+
+def create_app(env_name):
+  """
+  Create app
+  """
+
+  # app initiliazation
+  app = Flask(__name__)
+
+  app.config.from_object(app_config[env_name])
+
+  bcrypt.init_app(app)
+
+  db.init_app(app)
+
+  app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
+
+
+  @app.route('/', methods=['GET'])
+  def index():
+    """
+    example endpoint
+    """
+    return 'Congratulations! Your first endpoint is working'
+
+  return app
